96cb8f192ebff9031d7ad05d67608382bfb98525
gbrceregatti <gabrielblomer74@gmail.com>
2019-08-09 20:44:20 -0300

    AIFFplayer

---
+++ Problema1/src/problema1/AIFFPlayer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema1;
+
+/**
+ *
+ * @author User
+ */
+public class AIFFPlayer {
+
+    public AIFFPlayer(String nome) {
+    }
+    
+    public void play() {
+        
+    }
+    
+    public void stop() {
+    
+    }   
+    
+    public int pause() {
+        return 0;
+    }
+    
+    public void setCursor(int cursor) {
+    
+    }
+    
+    public void release() {
+        
+    }
+}
