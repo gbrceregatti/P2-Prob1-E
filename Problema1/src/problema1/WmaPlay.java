fb9c9170b7150bf4ed0eac713d5321adf1513c86
Henrique Alexsander Eichstadt <heichstadt@furb.br>
2019-08-09 20:43:34 -0300

    Adicionando classe WmaPlay

---
+++ Problema1/src/problema1/WmaPlay.java
@@ -0,0 +1,34 @@
+package problema1;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ *
+ * @author heichstadt
+ */
+public class WmaPlay {
+    
+    public void setFile(String text){
+        
+    }
+    
+    public void setLocation(int location){
+        
+    }
+    
+    public int getLocation(){
+        throw new NotImplementedException();
+    }
+    
+    public void open(){
+        
+    }
+    
+    public void play(){
+        
+    }
+    
+    public void stop(){
+        
+    }
+}
