ec8f4e7859af553f90d3ea98cef920e8335439ee
Gabriel Lucas de Pinho <gabriel_depinho@hotmail.com>
2019-08-16 21:15:02 -0300

    Novas classes adicionadas, Mapeamento incluido

---
+++ Problema1/src/problema1/etapa2/AACPlayer.java
@@ -0,0 +1,54 @@
+package problema1.etapa2;
+
+import problema1.etapa1.AudioFormat;
+
+public class AACPlayer implements AudioFormat {
+	
+	@Override
+    public void open(String nome) {
+        System.out.println("Carreguei o arquivo de audio na memória");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Iniciei a reprodução do arquivo de audio");
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("Pausando a reprodução do arquivo de audio");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Parando a reprodução e retornando ao início do arquivo");
+    }
+
+    @Override
+    public void forward(int tempo) {
+        System.out.println("Recebei como parâmetro a quantidade de\n"
+                + "segundos que devem ser avançados no arquivo a partir do ponto\n"
+                + "atual");
+    }
+
+    @Override
+    public void reward(int tempo) {
+        System.out.println("recebe como parâmetro a quantidade de segundos que devem ser\n"
+                + "retrocedidos no arquivo a partir do ponto atual.");
+    }
+
+    @Override
+    public void release() {
+        System.out.println("Fechei o arquivo de audio e liberei a memória");
+    }
+    
+    public void setLocation(int location){
+        System.out.println("Selecionei o local");
+    }
+    
+    public int getLocation(){
+        System.out.println("Local: 1");
+        return 1;
+    }
+
+}
